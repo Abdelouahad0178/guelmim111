a9af7bcf6dc47c5ca21868737aeb4a825b22188f
abdelouahad <120823323+abdouassa@users.noreply.github.com>
2025-03-18 06:39:02 +0000

    AB

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const gallery = document.getElementById("gallery");
-    const apiURL = "https://script.google.com/macros/s/AKfycby2U3GtWPthI__3Gr1jodIH3PElEz9HRVMMhZ86MC5YHo56v16RRst_CI-647hetmI/exec";  // Remplace par l'URL de ton script Google Apps Script
+    const apiURL = "https://script.google.com/macros/s/AKfycbw0QAsCtf_E3iBbhgv8MdyJH-s_b3GsZTq4VhXHloF4n6E-I2UABdipymUfAI6SoI-l/exec";  // Remplace par l'URL de ton script Google Apps Script
 
     fetch(apiURL)
         .then(response => response.json())
